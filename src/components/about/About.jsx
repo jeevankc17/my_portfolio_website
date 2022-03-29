68fa7677b5896c002b780ca256f4ffa49da82ec3
jeevan <jkc5186@gmail.com>
2022-03-29 09:58:13 +0545

    adding files to github pages

---
+++ src/components/about/About.jsx
@@ -0,0 +1,43 @@
+import "./about.css";
+import Award from "../../img/award.png";
+
+const About = () => {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className="a-desc">
+          A well designed website can make a great impression on others and establish 
+          trust and authority immediately while a poorly designed site could do the opposite.
+          This is why I’m here. My attention to detail and aesthetic touch are here to deliver 
+          the best version of your website or photos.
+        </p>
+        <div className="a-award">
+          <img src={Award} alt="" className="a-award-img" />
+          <div className="a-award-texts">
+            <h4 className="a-award-title">International Design Awards 2021</h4>
+            <p className="a-award-desc">
+              This was something very special happened to me.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/158518276/original/43ea348c0133b648f4ddaf839f8b8edbd0434c42/design-your-website-from-top-to-bottom-on-photoshop.jpg",
+    link: "https://moralstorynepal.blogspot.com/",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "https://moralstorynepal.blogspot.com/",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "https://moralstorynepal.blogspot.com/",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "https://moralstorynepal.blogspot.com/",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "https://moralstorynepal.blogspot.com/",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "https://moralstorynepal.blogspot.com/",
+  },
+];
